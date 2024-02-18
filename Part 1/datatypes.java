ad56fda2f4a0478fee3b9d6c66b3c84eb864d452
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-18 05:06:31 +0000

    FC for datypes.java for Part 1

---
+++ Part 1/datatypes.java
@@ -0,0 +1,69 @@
+public class App {
+    public static void main(String[] args) {
+
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        int myAge = 21;
+
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        float myGradeInClass = 3.00f;
+
+        // char - stores single characters, such as 'a' or 'B'. Char values are
+        // surrounded by single quotes
+        char myMiddleInitial = 'P';
+
+        // boolean - stores values with two states: true or false
+        boolean iLoveBasketball = true;
+        boolean iAmAProfessor = false;
+
+        // String - stores text, such as "Hello". String values are surrounded by double
+        // quotes
+        String myFirstName = "Jay Arre";
+
+        // If you don't want others (or yourself) to overwrite existing values, use the
+        // final keyword (this will declare the variable as "final" or "constant", which
+        // means unchangeable and read-only)
+        final String myLastName = "Talosig";
+
+        System.out.println(myAge);
+        System.out.println(myGradeInClass);
+        System.out.println(myMiddleInitial);
+        System.out.println(iLoveBasketball);
+        System.out.println(iAmAProfessor);
+        System.out.println(myFirstName);
+        System.out.println(myLastName);
+    }
+}public class App {
+    public static void main(String[] args) {
+
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        int myAge = 21;
+
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        float myGradeInClass = 3.00f;
+
+        // char - stores single characters, such as 'a' or 'B'. Char values are
+        // surrounded by single quotes
+        char myMiddleInitial = 'P';
+
+        // boolean - stores values with two states: true or false
+        boolean iLoveBasketball = true;
+        boolean iAmAProfessor = false;
+
+        // String - stores text, such as "Hello". String values are surrounded by double
+        // quotes
+        String myFirstName = "Jay Arre";
+
+        // If you don't want others (or yourself) to overwrite existing values, use the
+        // final keyword (this will declare the variable as "final" or "constant", which
+        // means unchangeable and read-only)
+        final String myLastName = "Talosig";
+
+        System.out.println(myAge);
+        System.out.println(myGradeInClass);
+        System.out.println(myMiddleInitial);
+        System.out.println(iLoveBasketball);
+        System.out.println(iAmAProfessor);
+        System.out.println(myFirstName);
+        System.out.println(myLastName);
+    }
+}
