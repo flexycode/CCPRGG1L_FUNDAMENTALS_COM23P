4eba3bb9f0a502ecf0f4f943ea70bb1fa32e227c
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-19 15:37:34 +0000

    Added while_statement

---
+++ Part 6/iteration_structures/while_statement/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int rideCount = 10;
+
+        while (rideCount != 0) {
+            System.out.println("Demacia!.. My will be done.");
+            rideCount--;
+        }
+    }
+}
