80b448f673730646dc84c1ef0956ddc19f8b87e5
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-19 15:42:25 +0000

    Added sequential_structures

---
+++ Part 6/sequential_structures/App.java
@@ -0,0 +1,46 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Sequential Structure
+        // Methods are called in order.
+
+        // Step 1
+        rideJeep();
+
+        // Step 2
+        rideBus();
+
+        // Step 3
+        rideLRT();
+
+        // Step 4
+        rideTricycle();
+
+        // Step 5
+        walk();
+    }
+
+    static void rideJeep() {
+        System.out.println("Ride jeep along katipunan Street");
+        System.out.println("Exit jeep along Sumulong Highway");
+    }
+
+    static void rideBus() {
+        System.out.println("Ride bus along Sumulong Highway");
+        System.out.println("Exit bus at SM Masinag");
+    }
+
+    static void rideLRT() {
+        System.out.println("Ride at Antipolo station");
+        System.out.println("Exit at Legarda station");
+    }
+
+    static void rideTricycle() {
+        System.out.println("Ride tricycle at Figueras Street");
+        System.out.println("Exit tricycle at Jhocson Street");
+    }
+
+    static void walk() {
+        System.out.println("Walk for 3 mins");
+    }
+}
