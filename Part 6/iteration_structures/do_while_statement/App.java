ced62a28b962321aa0f80107014a2e091b18f656
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-19 15:31:34 +0000

    Added iteration_structures folder for do_while_statement

---
+++ Part 6/iteration_structures/do_while_statement/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int rideCount = 10;
+        do {
+            System.out.println("Demacia!.. My heart and sword always for Demacia!");
+            rideCount--;
+        } while (rideCount != 0);
+    }
+}
