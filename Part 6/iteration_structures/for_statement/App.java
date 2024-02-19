44fd72b6d54151a46a88eea708eafdcdcf747488
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-19 15:34:15 +0000

    Added for_statement

---
+++ Part 6/iteration_structures/for_statement/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 10; i != 0; i--) {
+            System.out.println("Demacia!.. Fear is the first of many foes.");
+        }
+
+    }
+}
