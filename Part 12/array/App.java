bbbc8908ab70deab0476f937be54ac1c5f19c27b
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-20 04:15:59 +0000

    Added array in Part 12

---
+++ Part 12/array/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Array of Strings
+        String[] myfavefastfood = { "LukaDoncic", "JohnWall", "AllenIverson", "KyrieIrving", 
+        "DamianLillard" };
+
+        // Array of Integers
+        int[] primeNumbers = { 2, 3, 5, 7 };
+
+        // Array of Characters
+        char[] myName = { 'J', 'A', 'Y', 'A', 'R', 'R', 'E' };
+    }
+}
