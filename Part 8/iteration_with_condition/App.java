be3bb95c898fb2b8b771b480167ff563c8558ce5
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-20 08:00:33 +0000

    Added for and iteration_with_condition

---
+++ Part 8/iteration_with_condition/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 5; i != 0; i--) {
+
+            if (i == 5) {
+                System.out.println("Demaciaaaa!");
+            } else if (i == 4) {
+                System.out.println("Noxussss!!");
+            } else if (i == 3) {
+                System.out.println("Shurimaaaaaa!!!");
+            } else if (i == 2) {
+                System.out.println("Ioniaaaaaaa!!!!");
+            } else if (i == 1) {
+                System.out.println("one last time!!!!!");
+            }
+        }
+
+    }
+}
