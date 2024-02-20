f018f997a47017f5060d7adeea39aab1126c9b42
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-20 07:55:05 +0000

    Added continue and do_while

---
+++ Part 8/do_while/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int count = 5;
+
+        do {
+            System.out.println("Jay");
+            count--;
+        } while (count != 0);
+    }
+}
