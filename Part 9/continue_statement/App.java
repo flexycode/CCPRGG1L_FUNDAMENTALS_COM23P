6fbca9cc6562766d58303b1679a3e4c582d39846
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-02-22 01:01:15 +0000

    Added   continue_statement

---
+++ Part 9/continue_statement/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 5; i != 0; i--) {
+
+            if (i == 5) {
+                System.out.print("Garen ");
+            } else if (i == 4) {
+                System.out.print("Jarvan! ");
+            } else if (i == 3) {
+                System.out.print("XinZhao ");
+                System.out.println(" ");
+                continue;
+            } else if (i == 2) {
+                System.out.print("Galio ");
+            } else if (i == 1) {
+                System.out.print("Lux ");
+            }
+
+            System.out.println("Demacia!");
+        }
+    }
+}
